3e83d96cc61297a7a43509e025aa636ff6f0934c
Murkrow <ducdt1992@gmail.com>
2017-06-01 15:36:51 +0700

    + add username

--- app/components/ProfilePage/index.js
+++ app/components/ProfilePage/index.js
@@ -6,6 +6,8 @@ import React, {Component} from "react";
 import PageWrapper from "../Commons/Wrapper";
 import TopNavigationBarWithSideBar from "../Commons/TopNavigationBar/TopNavigationBarWithSideBar";
 import colors from "../../styles/colors";
+import Background from "../../styles/images/bg.jpg";
+import {Image, Text} from "react-native";
 
 export default class ProfilePage extends Component {
 
@@ -17,6 +19,8 @@ export default class ProfilePage extends Component {
                     title="Cá nhân"
                     rightButton={rightButton}
                     onLeftButtonPress={() => navigate('DrawerOpen')}/>
+                <Image style={styles.avatar} source={Background}/>
+                <Text style={styles.username}>'Đoàn Phúc Bảo'</Text>
             </PageWrapper>
         );
     }
@@ -25,4 +29,19 @@ export default class ProfilePage extends Component {
 const rightButton = {
     title: 'Lưu lại',
     tintColor: colors.blue,
+};
+
+const styles = {
+    avatar: {
+        marginTop: 16,
+        alignSelf: 'center',
+        borderWidth: 1,
+        width: 120,
+        height: 120,
+        borderRadius: 100,
+    },
+    username: {
+        marginTop: 8,
+        alignSelf: 'center'
+    }
 };
