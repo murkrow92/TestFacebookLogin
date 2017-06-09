d0703af0502a180056a71eb9d0afcf5c3b0c92f3
murkrow <ducdt1992@gmail.com>
2017-06-09 23:06:08 +0700

    + add images

---
+++ app/components/Commons/ListAstroObject/AstroObjectItem.js
@@ -0,0 +1,22 @@
+/**
+ * Created by murkrow on 6/9/17.
+ */
+
+import {Text, View} from "react-native";
+import React, {Component, PropTypes} from "react";
+import colors from "../../styles/colors";
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class AstroObjectItem extends Component {
+
+    static propTypes = {
+        data: PropTypes.object.isRequired
+    };
+
+    render() {
+        return (<View>
+
+        </View>)
+    }
+}
+
