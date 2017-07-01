7333c812b1d3fe2ba42820dd2c2a33c0544a9c51
murkrow <ducdt1992@gmail.com>
2017-07-01 14:58:18 +0700

    + add sidebar reducer

--- app/components/Sidebar/index.js
+++ app/components/Sidebar/index.js
@@ -7,13 +7,19 @@ import {View, StyleSheet} from "react-native";
 import AccountBox from "./AccountBox";
 import DrawerItem from "./DrawerItem";
 import DrawerTitle from "./DrawerTitle";
+import {bindActionCreators} from "redux";
+import * as actions from "./SideBarAction";
+import {connect} from "react-redux";
 
-export default class Sidebar extends Component {
+class Sidebar extends Component {
     constructor(props) {
         super(props);
+        const {actions} = this.props;
+        actions.getLocalProfileAsync();
     }
 
     render() {
+        const {actions, sidebar} = this.props;
         const {navigate} = this.props.content.navigation;
         return ( <View style={styles.container}>
             <AccountBox
@@ -49,3 +55,15 @@ export default class Sidebar extends Component {
 const styles = StyleSheet.create({
     container: {}
 });
+
+const mapStateToProps = (state) => ({
+    sidebar: state.sidebar,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators(actions, dispatch)
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
+
--- app/rootReducer.js
+++ app/rootReducer.js
@@ -6,9 +6,11 @@ import {combineReducers} from 'redux';
 import appReducer from "./appReducer";
 import profileReducer from "./components/ProfilePage/ProfileReducer";
 import homeReducer from "./components/HomePage/HomeReducer";
+import sidebarReducer from "./components/Sidebar/SideBarReducer";
 
 export default combineReducers({
     app: appReducer,
     profile: profileReducer,
-    home:homeReducer
+    home:homeReducer,
+    sidebar:sidebarReducer
 });
